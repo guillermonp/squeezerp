1526906abd8a1f2516d748a005121144651cb1c8
GuillermoNP <guillermo.navas.89@gmail.com>
2014-06-04 23:35:21 +0200

    new insert_*

--- squeezerp/database/database_core.py
+++ squeezerp/database/database_core.py
@@ -5,7 +5,7 @@ from squeezerp import resources
 from squeezerp.database import database_queries
 
 
-class Database:
+class Database(object):
     def __init__(self):
         self.db_name = resources.DATABASE
         self._connection = sqlite3.connect(self.db_name)
@@ -42,15 +42,90 @@ class DatabaseOperations(Database):
         - DELETE (deleting rows from a table)
     """
     def __init__(self):
-        Database.__init__(self)
+        super(Database, self).__init__()
+
+    def sql(self, query):
+        """
+        Run custom queries:
+            db = DatabaseOperations()
+            db.sql("select * from table")
+            db.sql("select * from {0}".format(table))
+            ...
+        """
+        self._execute_query(query)
 
     def create_table(self, query):
+        """" create a new table """
         return self._execute_query(query)
 
     def create_all_tables(self, tables):
+        """
+        create a set of tables:
+            this methods will be used during the set up and will create all those tables
+             to run the application.
+        """
         for table in tables:
             self._execute_query(table)
+
+    def insert_initial_tables(self, operations):
+        """
+        insert values to some fixed tables:
+            ItemTypes
+            DataUploaderHistoryStatus
+            DataUploaderHistoryFormats
+        """
+        for op in operations:
+            self._execute_query(op)
 
     def insert_history_uploader(self, fields):
         query = database_queries.insert_datauploader
         self._execute_query(query, fields)
+
+    def insert_category(self, c_id, c_name, c_description=None):
+        """
+        Add new category to table Categories
+
+        :param c_id: Categories.id
+        :param c_name: Categories.name
+        :param c_description: Categories.description
+        """
+        fields = (c_id, c_name, c_description)
+        query = database_queries.insert_category
+        self._execute_query(query=query, param=fields)
+
+    def insert_family(self, f_id, f_name, c_id, f_description=None):
+        """
+        Add new family to table Families
+
+        :param f_id: Families.id
+        :param f_name: Families.name
+        :param c_id: Families.category_id
+        :param f_description: Families.description
+        """
+        fields = (f_id, f_name, c_id, f_description)
+        query = database_queries.insert_family
+        self._execute_query(query=query, param=fields)
+
+    def insert_vat(self, v_name, value, eq):
+        """
+        Add new Value-added tax to table VAT
+
+        :param v_name: VAT.name
+        :param value: VAT.value
+        :param eq: VAT.equalisation_tax
+        """
+        fields = (v_name, value, eq)
+        query = database_queries.insert_vat
+        self._execute_query(query=query, param=fields)
+
+    def insert_warehouse(self, w_name, w_description, w_location):
+        """
+        Add new warehouse to table Warehouses
+
+        :param w_name: Warehouses.name
+        :param w_description: Warehouses.description
+        :param w_location: Warehouses.location
+        """
+        fields = (w_name, w_description, w_location)
+        query = database_queries.insert_warehouse
+        self._execute_query(query=query, param=fields)
