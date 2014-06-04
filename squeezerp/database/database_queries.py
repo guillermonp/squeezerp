8f7f6c58930e0374edf50741a86ce069a87940d3
GuillermoNP <guillermo.navas.89@gmail.com>
2014-06-04 18:42:45 +0200

    cast (file_size) datauploader history

--- squeezerp/database/database_queries.py
+++ squeezerp/database/database_queries.py
@@ -13,6 +13,7 @@ def create_tables():
 def insert_initial():
     return [insert_items_type, insert_datauploader_status, insert_datauploader_formats]
 
+
 create_categories = """
                     CREATE TABLE Categories(
                     id INTEGER PRIMARY KEY ASC NOT NULL,
@@ -31,13 +32,13 @@ create_families = """
                     """
 
 create_VAT = """
-                CREATE TABLE VAT(
-                id INTEGER PRIMARY KEY NOT NULL,
-                name TEXT NOT NULL,
-                value REAL NOT NULL,
-                equalisation_tax REAL NOT NULL,
-                UNIQUE(name, value, equalisation_tax) ON CONFLICT ROLLBACK)
-                """
+                    CREATE TABLE VAT(
+                    id INTEGER PRIMARY KEY NOT NULL,
+                    name TEXT NOT NULL,
+                    value REAL NOT NULL,
+                    equalisation_tax REAL NOT NULL,
+                    UNIQUE(name, value, equalisation_tax) ON CONFLICT ROLLBACK)
+                    """
 
 create_warehouses = """
                     CREATE TABLE Warehouses(
@@ -49,31 +50,31 @@ create_warehouses = """
                     """
 
 create_datatools_history = """
-                            CREATE TABLE DataUploaderHistory(
-                            id INTEGER PRIMARY KEY NOT NULL,
-                            sheet_name TEXT NOT NULL,
-                            file_name TEXT NOT NULL,
-                            file_size INTEGER NOT NULL,
-                            file_format INTEGER NOT NULL,
-                            has_error INTEGER NOT NULL,
-                            records_found INTEGER NOT NULL,
-                            errors INTEGER NOT NULL,
-                            status INTEGER NOT NULL,
-                            start TEXT NOT NULL,
-                            end TEXT NOT NULL)
-                            """
+                    CREATE TABLE DataUploaderHistory(
+                    id INTEGER PRIMARY KEY NOT NULL,
+                    sheet_name TEXT NOT NULL,
+                    file_name TEXT NOT NULL,
+                    file_size INTEGER NOT NULL,
+                    file_format INTEGER NOT NULL,
+                    has_error INTEGER NOT NULL,
+                    records_found INTEGER NOT NULL,
+                    errors INTEGER NOT NULL,
+                    status INTEGER NOT NULL,
+                    start TEXT NOT NULL,
+                    end TEXT NOT NULL)
+                    """
 
 create_datatools_history_status = """
-                                    CREATE TABLE DataUploaderHistoryStatus(
-                                    id INTEGER PRIMARY KEY NOT NULL,
-                                    status_msg TEXT NOT NULL)
-                                    """
+                    CREATE TABLE DataUploaderHistoryStatus(
+                    id INTEGER PRIMARY KEY NOT NULL,
+                    status_msg TEXT NOT NULL)
+                    """
 
 create_datatools_history_formats = """
-                                    CREATE TABLE DataUploaderHistoryFormats(
-                                    id INTEGER PRIMARY KEY NOT NULL,
-                                    input_format TEXT NOT NULL)
-                                    """
+                    CREATE TABLE DataUploaderHistoryFormats(
+                    id INTEGER PRIMARY KEY NOT NULL,
+                    input_format TEXT NOT NULL)
+                    """
 
 create_itemTypes = """
                     CREATE TABLE ItemTypes(
@@ -98,49 +99,49 @@ insert_items_type = """
                     """
 
 insert_datauploader_status = """
-                            INSERT INTO DataUploaderHistoryStatus
-                                SELECT 0 AS 'id', 'successfully uploaded' AS 'status_msg'
-                            UNION SELECT 1, 'error reading csv file'
-                            UNION SELECT 2, 'number of columns for uploaded sheet is incorrect'
-                            UNION SELECT 3, 'there are records with errors'
-                            """
+                    INSERT INTO DataUploaderHistoryStatus
+                        SELECT 0 AS 'id', 'successfully uploaded' AS 'status_msg'
+                    UNION SELECT 1, 'error reading csv file'
+                    UNION SELECT 2, 'number of columns for uploaded sheet is incorrect'
+                    UNION SELECT 3, 'there are records with errors'
+                    """
 
 insert_datauploader_formats = """
-                                INSERT INTO DataUploaderHistoryFormats
-                                    SELECT 0 AS 'id', 'csv' AS 'input_format'
-                                UNION SELECT 1, 'xls'
-                                """
+                    INSERT INTO DataUploaderHistoryFormats
+                        SELECT 0 AS 'id', 'csv' AS 'input_format'
+                    UNION SELECT 1, 'xls'
+                    """
 
 insert_warehouse = 'INSERT INTO Warehouses (name, description, location) VALUES (?, ?, ?)'
 
 insert_datauploader = """
-                        INSERT INTO DataUploaderHistory
-                        (sheet_name, file_name, file_size, file_format, has_error, records_found, errors, status,
-                        start, end) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                        """
+                    INSERT INTO DataUploaderHistory
+                    (sheet_name, file_name, file_size, file_format, has_error, records_found, errors, status,
+                    start, end) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                    """
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # READ
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 select_datauploader_history = """
-                            SELECT
-                                du.id,
-                                du.sheet_name AS [uploaded sheet],
-                                du.file_name AS [file],
-                                du.file_size AS [file size(kB)],
-                                duf.input_format AS [file type],
-                                du.has_error AS [has error],
-                                du.records_found AS [records],
-                                du.errors AS [#errors],
-                                dus.status_msg AS [status],
-                                du.start,
-                                du.end
-                            FROM DataUploaderHistory du
-                                INNER JOIN DataUploaderHistoryStatus dus
-                                ON du.status = dus.id
-                                INNER JOIN DataUploaderHistoryFormats duf
-                                ON du.file_format = duf.id
-                            ORDER BY
-                                du.id ASC
-                            """
+                    SELECT
+                        du.id,
+                        du.sheet_name AS [uploaded sheet],
+                        du.file_name AS [file],
+                        CAST(du.file_size AS REAL) / 1024 AS [file size(kB)],
+                        duf.input_format AS [file type],
+                        du.has_error AS [has error],
+                        du.records_found AS [records],
+                        du.errors AS [#errors],
+                        dus.status_msg AS [status],
+                        du.start,
+                        du.end
+                    FROM DataUploaderHistory du
+                        INNER JOIN DataUploaderHistoryStatus dus
+                        ON du.status = dus.id
+                        INNER JOIN DataUploaderHistoryFormats duf
+                        ON du.file_format = duf.id
+                    ORDER BY
+                        du.id ASC
+                    """
