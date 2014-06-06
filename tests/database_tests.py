f046336819160088e5367eea401c3aaa891a92c8
GuillermoNP <guillermo.navas.89@gmail.com>
2014-06-06 23:57:29 +0200

    new test

--- tests/database_tests.py
+++ tests/database_tests.py
@@ -1,6 +1,7 @@
 from squeezerp.database.database_core import DatabaseOperations
 from squeezerp.database import database_queries
+from squeezerp import resources
 
-db = DatabaseOperations()
-db.create_all_tables(database_queries.create_tables())
-db.close()
+DatabaseOperations().sql_script(resources.DATABASE_SCHEMA)
+DatabaseOperations().insert_initial_data(database_queries.insert_initial())
+DatabaseOperations().sql("select * from table", " where field1 = '2'", " order by field1 desc")
