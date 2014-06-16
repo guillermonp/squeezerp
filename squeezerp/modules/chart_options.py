496f6b6a044948c34b8fc1e904ebdfee47dbd13a
GuillermoNP <guillermo.navas.89@gmail.com>
2014-06-16 18:19:53 +0200

    chart options colors

---
+++ squeezerp/modules/chart_options.py
@@ -0,0 +1,3 @@
+
+chart_colors = {"dark_grey": ["#1C1C1C", "label color"],
+                "grey": ["#585858", "axes and tick color"]}
--- squeezerp/modules/charts.py
+++ squeezerp/modules/charts.py
@@ -1,14 +1,14 @@
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as Canvas
 from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
-
+from squeezerp.modules.chart_options import chart_colors
 
 class PlotWidget(Canvas):
-    def __init__(self, parent):
+    def __init__(self, parent, x_dimension, y_dimension):
         super(PlotWidget, self).__init__(Figure())
 
         self.setParent(parent)
-        self.figure = Figure((7.0, 2.0))
+        self.figure = Figure((x_dimension, y_dimension))
         self.canvas = Canvas(self.figure)
 
         self.axes = self.figure.add_subplot(111)
@@ -30,4 +30,21 @@ class PlotWidget(Canvas):
         self.figure.tight_layout()
 
     def plot_style(self):
-        self.axes.spines['bottom'].set_color('grey')
+
+        # change axes colors - grey
+        axes = ["bottom", "top", "left", "right"]
+        for ax in axes:
+            self.axes.spines[ax].set_color(chart_colors["grey"][0])
+
+        # change x-label and y-label color
+        self.axes.xaxis.label.set_color(chart_colors["dark_grey"][0])
+        self.axes.yaxis.label.set_color(chart_colors["dark_grey"][0])
+
+        # change tick color
+        self.axes.tick_params(axis='x', colors=chart_colors["grey"][0])
+        self.axes.tick_params(axis='y', colors=chart_colors["grey"][0])
+
+        # change font size
+
+        # add grid - soft grey
+        self.axes.grid(True)
