ad796dd8f8ecd38140c8cde899b45674ff4f5bf9
GuillermoNP <guillermo.navas.89@gmail.com>
2014-06-20 00:20:56 +0200

    new signals

--- squeezerp/gui/dialogs/datatools_history/controller.py
+++ squeezerp/gui/dialogs/datatools_history/controller.py
@@ -4,17 +4,18 @@ from squeezerp.gui import ui_tools
 from squeezerp.gui.dialogs.datatools_history.view import ViewDataToolsHistory
 
 
-class ControllerDataTools(ViewDataToolsHistory):
+class ControllerDataToolsHistory(ViewDataToolsHistory):
     def __init__(self):
         ViewDataToolsHistory.__init__(self)
 
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # connections and slots
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        self.connect(self.btn_view, QtCore.SIGNAL("clicked()"), self.view_table)
+        self.connect(self.rbtn_by_errors, QtCore.SIGNAL("toggled(bool)"), self.view_group)
+        self.connect(self.cbo_period, QtCore.SIGNAL("currentIndexChanged(const QString&)"), self.view_table)
 
         # import core functions to print as pdf file or export as xls or csv files
-        self.connect(self.export_action, QtCore.SIGNAL("triggered()"), )
-        self.connect(self.print_action, QtCore.SIGNAL("triggered()"), )
+        #self.connect(self.export_action, QtCore.SIGNAL("triggered()"), )
+        #self.connect(self.print_action, QtCore.SIGNAL("triggered()"), )
 
         self.worker_thread = ui_tools.WorkerThread()
