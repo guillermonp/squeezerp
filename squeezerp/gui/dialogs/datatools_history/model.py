01f6df82aec1fce5a69350cc1ae4fd5bb1c9c2d6
GuillermoNP <guillermo.navas.89@gmail.com>
2014-06-19 20:54:15 +0200

    period filter

--- squeezerp/gui/dialogs/datatools_history/model.py
+++ squeezerp/gui/dialogs/datatools_history/model.py
@@ -1,10 +1,12 @@
 import sys
 from PyQt4.QtGui import QApplication
 
-from squeezerp.database import database_core as dbcore, database_queries as db_q
+from squeezerp.database import database_core as db_core
+from squeezerp.database import database_queries as db_q
 from squeezerp.gui import ui_tools
-from squeezerp.gui.dialogs.datatools_history.controller import ControllerDataToolsHistory
+
 from squeezerp.gui.dialogs.datatools_history import app_data
+from squeezerp.gui.dialogs.datatools_history.controller import ControllerDataToolsHistory
 
 
 class ModelDataToolsHistory(ControllerDataToolsHistory):
@@ -12,24 +14,29 @@ class ModelDataToolsHistory(ControllerDataToolsHistory):
         ControllerDataToolsHistory.__init__(self)
 
         # DataUploaderHistory variables
-        self.db = dbcore.DatabaseOperations()
+        self.db = db_core.DatabaseOperations()
+        self.query = db_q.select_datauploader_history
 
         # initialize with period = 1 week
         self.period = 1
-        self.query = db_q.select_datauploader_history
-        self.snippet_query = db_q.snippet_du_history_period_1week
+        self.snippet = db_q.snippet_du_history_period_1week
 
-        # add combobox periods
+        # add combobox periods - set combobox = 1 week
         ui_tools.load_combobox(self.cbo_period, app_data.PERIODS.values())
+        _index = self.cbo_period.findText(app_data.PERIODS[self.period])
+        self.cbo_period.setCurrentIndex(_index)
 
         # show initial data
-
         self.view_table()
 
     def view_table(self):
         """ visualize table DataUploaderHistory """
+        period_name = str(self.cbo_period.currentText())
+        self.period = [k for k, v in app_data.PERIODS.iteritems() if v == period_name][0]
 
-        headers, data = dbcore.DatabaseOperations().sql_fetch_data(self.query, self.snippet_query)
+        self.period_filter()
+        headers, data = self.db.sql_fetch_data(self.query, self.snippet)
+
         ui_tools.view_table(self.tbl_uploads, headers, data)
 
     def view_group(self):
@@ -40,6 +47,18 @@ class ModelDataToolsHistory(ControllerDataToolsHistory):
             no group: show all data.
         """
         pass
+
+    def period_filter(self):
+        if self.period == 0:
+            self.snippet = db_q.snippet_du_history_period_today
+        elif self.period == 1:
+            self.snippet = db_q.snippet_du_history_period_1week
+        elif self.period == 2:
+            self.snippet = db_q.snippet_du_history_period_1month
+        elif self.period == 3:
+            self.snippet = db_q.snippet_du_history_period_3month
+        else:
+            self.snippet = None  # no snippets
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
