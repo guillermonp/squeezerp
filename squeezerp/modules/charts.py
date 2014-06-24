05b19dd5dbfe497f8f85c71113e11c6d2f9a976e
GuillermoNP <guillermo.navas.89@gmail.com>
2014-06-25 01:41:33 +0200

    plot_lines()

--- squeezerp/modules/charts.py
+++ squeezerp/modules/charts.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from matplotlib.figure import Figure
 from matplotlib import rc
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as Canvas
@@ -6,7 +8,7 @@ from squeezerp.modules.chart_options import chart_colors, chart_font
 
 
 class PlotWidget(Canvas):
-    def __init__(self, parent, x_dimension, y_dimension, x_label=None, y_label=None):
+    def __init__(self, parent, x_dimension, y_dimension):
         super(PlotWidget, self).__init__(Figure())
 
         self.setParent(parent)
@@ -22,16 +24,26 @@ class PlotWidget(Canvas):
         # background color = white
         self.figure.set_facecolor('white')
 
-        self.plot_properties(x_label, y_label)
+        # global variables
+        self.x_limit = None
+
         self.plot_style()
 
-    def plot_properties(self, x_label, y_label):
+    def plot_properties(self, x_label, y_label, x_limit=None):
+        """
+        Plot properties and variables
+            axis labels
+            x axis label (limit)
+        :param x_label: x axis labels
+        :param y_label: y axis labels
+        :param x_limit: number of x axis labels to display
+        """
 
         if x_label and y_label is not None:
             self.axes.set_xlabel(x_label)
             self.axes.set_ylabel(y_label)
 
-        self.figure.tight_layout()
+            self.x_limit = x_limit
 
     def plot_style(self):
 
@@ -54,5 +66,40 @@ class PlotWidget(Canvas):
         # add grid - soft grey
         self.axes.grid(True)
 
-    def plot(self):
+    def plot_lines(self, x_values, y_values, *args):
+
+        # number of columns - range
+        columns = len(x_values)
+        ind = np.arange(columns)
+
+        # convert x_values to string and y_values to float - review!
+        x_labels = np.array(x_values, dtype=str)
+        y = np.array(y_values, dtype=float)
+
+        # tick labels with dates
+        if columns <= self.x_limit:
+            self.axes.set_xticks(np.arange(columns))
+
+        self.axes.xaxis.get_majorticklocs()
+        self.axes.set_xticklabels(x_labels)
+
+        # show y_values
+        if not args:
+            self.axes.plot(ind, y)
+        else:
+            # multiple series
+            pass
+
+        self.figure.tight_layout()
+
+    def plot_bars(self):
+        pass
+
+    def plot_stats(self):
+        pass
+
+    def plot_pie(self):
+        pass
+
+    def plot_polar(self):
         pass
