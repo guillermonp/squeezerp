612609f1c26e6ce4a745e9813cf2ed2cc04f549e
GuillermoNP <guillermo.navas.89@gmail.com>
2014-06-25 02:06:11 +0200

    periods update

--- squeezerp/gui/dialogs/datatools_history/app_data.py
+++ squeezerp/gui/dialogs/datatools_history/app_data.py
@@ -1,8 +1,8 @@
 
-periods = {
-    "1 day": "Period: 1 day",
-    "1 week": "Period: 1 week",
-    "1 month": "Period: 1 month",
-    "3 months": "Period: 3 months",
-    "all": "All periods"
+PERIODS = {
+    0: "Period: 1 day",
+    1: "Period: 1 week",
+    2: "Period: 1 month",
+    3: "Period: 3 months",
+    4: "All periods"
 }
